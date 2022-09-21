248663c24fb3b774c8fb73ccd40fc439d74210ca
bollineniv <bolinenibv@gmail.com>
2022-09-21 14:56:22 -0400

    initial push

---
+++ src/main/java/com/yvault/yvault/YvaultApplication.java
@@ -0,0 +1,13 @@
+package com.yvault.yvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yvault/yvault/YvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvault.yvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
